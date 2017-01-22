e03e32384625b2e338b9066e1c38cc9a33ccbe42
shaudai <shaudai0402@hotmail.com>
2017-01-22 23:28:35 +0800

    Add main menu for adding note

--- app/src/main/java/com/garfieldchou/notes/MainActivity.java
+++ app/src/main/java/com/garfieldchou/notes/MainActivity.java
@@ -2,8 +2,29 @@ package com.garfieldchou.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        Toast.makeText(this, "Add note", Toast.LENGTH_SHORT).show();
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
