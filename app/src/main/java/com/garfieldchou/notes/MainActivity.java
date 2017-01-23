a9b1614dac9f995a4baa15f7c16abb1983dd0579
garfield_chou <garfield_chou@asus.com>
2017-01-23 11:15:35 +0800

    Add ArrayList for ListView

--- app/src/main/java/com/garfieldchou/notes/MainActivity.java
+++ app/src/main/java/com/garfieldchou/notes/MainActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    static ArrayList<String> noteList = new ArrayList<>();
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -30,5 +36,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView noteListView = (ListView) findViewById(R.id.noteListView);
+
+        noteList.add("Example note");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, noteList);
+
+        noteListView.setAdapter(arrayAdapter);
+
     }
 }
