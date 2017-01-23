f332b5d752348fbc152e0cf842c09bb828f2bcbf
shaudai <shaudai0402@hotmail.com>
2017-01-23 23:28:39 +0800

    Use addTextChangedListener and TextWatcher to update notes

--- app/src/main/java/com/garfieldchou/notes/MainActivity.java
+++ app/src/main/java/com/garfieldchou/notes/MainActivity.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     static ArrayList<String> notes = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         notes.add("Example note");
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
 
         listView.setAdapter(arrayAdapter);
 
